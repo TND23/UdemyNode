5d4517557adb5eb4438b62dc7579b1136c164ba6
tnd23 <thomas.nast23@gmail.com>
2020-01-17 19:24:17 -0800

    Add Google OAuth

--- server/index.js
+++ server/index.js
@@ -9,6 +9,11 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
+// client id 368692868574-qq6jhvko34d15lcqf64uq7orua3s5gce.apps.googleusercontent.com
+// client secret -IqJ2GovIjo3DYO5Tt0azhv2
+
+passport.use(new GoogleStrategy());
+
 // TODO: learn about arrow function
 // register router handler to app:
 
